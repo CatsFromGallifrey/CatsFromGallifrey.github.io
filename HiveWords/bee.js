2680521a88271e8839450fff47f2500746978946
CatsFromGallifrey <ldileonardo98@gmail.com>
2025-01-29 11:37:56 +0100

    Update bee.js

--- HiveWords/bee.js
+++ HiveWords/bee.js
@@ -1,6 +1,6 @@
 // spellingBee.js
 
-const letters = ['H', 'C', 'A', 'L', 'B', 'E', 'Y']; // Lettere di esempio
+const letters = ['V', 'C', 'A', 'L', 'N', 'E', 'O']; // Lettere di esempio
 const centralLetter = letters[0]; //'A'; // Lettera centrale obbligatoria
 let outerLetters = letters.slice(1);
 // const validWords = ['AB', 'AC', 'AD', 'AE', 'AF', 'AG']; // Parole valide di esempio
