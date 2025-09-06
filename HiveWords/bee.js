0c9d13106845594de77537c6a081cdcc584bee0d
CatsFromGallifrey <ldileonardo98@gmail.com>
2025-09-06 15:45:45 +0200

    Update bee.js

--- HiveWords/bee.js
+++ HiveWords/bee.js
@@ -1,6 +1,6 @@
 // spellingBee.js
 
-const letters = ['G', 'A', 'R', 'U', 'T', 'N', 'O']; // Lettere di esempio
+const letters = ['V', 'L', 'E', 'P', 'D', 'C', 'O']; // Lettere di esempio
 const centralLetter = letters[0]; //'A'; // Lettera centrale obbligatoria
 let outerLetters = letters.slice(1);
 // const validWords = ['AB', 'AC', 'AD', 'AE', 'AF', 'AG']; // Parole valide di esempio
